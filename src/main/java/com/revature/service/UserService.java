d75231f3d2b76d0ded64acfd8b0c18c633a3134e
nfrederick023 <nfrederick023@gmail.com>
2021-11-30 18:54:56 -0500

    Added Login and Registration
    Created a new Account Controller, which will handle login, logout, and registration. Currently only login and register are implemented.
    
    I added the register endpoint for validating and registering users.  Currently only tests against duplicate usernames.
    
    I removed createNewUser() from the User Controller since that is now handled by Account Controller.
    
    I created Account Service to service any register, logout, and login logic on the backend.

--- src/main/java/com/revature/service/UserService.java
+++ src/main/java/com/revature/service/UserService.java
@@ -1,11 +1,5 @@
 package com.revature.service;
 
-// Spring Stereotypes
-// @Component - general use annotation to wire up a class as an annotation
-// @Service - more specific annotation to wire up a Service
-// @Controller - specific annotation to wire up a Controller (Servlet)
-// @Repository - specific annotation to wire up a Repository (DAO)
-
 import com.revature.model.User;
 import com.revature.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -27,12 +21,5 @@ public class UserService {
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
-    }
-
-    public User validateUser(User user) {
-        if (userRepository.findByUsername(user.getUsername()) != null && userRepository.findByUsername(user.getUsername()).getPassword().equals(user.getPassword())) {
-            return userRepository.findByUsername(user.getUsername());
-        }
-        return null;
     }
 }
