2a2c086c213cb62e2fcdd4a42cb27dad3e25fda9
nfrederick023 <nfrederick023@gmail.com>
2021-11-29 19:25:08 -0500

    Init Commit

---
+++ src/main/java/com/revature/Amplifire_RoadTrip/AmplifireRoadTripApplication.java
@@ -0,0 +1,13 @@
+package com.revature.Amplifire_RoadTrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmplifireRoadTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmplifireRoadTripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/Amplifire_RoadTrip/AmplifireRoadTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.Amplifire_RoadTrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmplifireRoadTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
