81786d2942873c77c21c11df6183e5379d924eff
Isaiah <isaiahsanason@gmail.com>
2021-12-01 21:39:00 -0800

    Fixed silly mistake and fixed sequences so they are unique for each table

--- src/main/java/com/revature/model/User.java
+++ src/main/java/com/revature/model/User.java
@@ -14,7 +14,8 @@ import java.util.List;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO,generator="user_generator")
+    @SequenceGenerator(name="user_generator", sequenceName = "user_seq")
     private int userId;
 
     @Column(unique = true)
