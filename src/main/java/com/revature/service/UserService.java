a2247c443fa558c0f0962b00d00633f3306e6a64
Justin Sherfey <35387786+Justin-Sherfey@users.noreply.github.com>
2021-12-07 12:16:30 -0800

    Update UserService.java

--- src/main/java/com/revature/service/UserService.java
+++ src/main/java/com/revature/service/UserService.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Service
 public class UserService {
 
-    private static UserRepository userRepository = null;
+    private UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
