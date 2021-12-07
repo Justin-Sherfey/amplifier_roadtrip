204ea1c67f9feea29c2f2cfe688f61d82dda4647
Justin Sherfey <35387786+Justin-Sherfey@users.noreply.github.com>
2021-12-07 12:47:34 -0800

    Update UserRepository.java

--- src/main/java/com/revature/repository/UserRepository.java
+++ src/main/java/com/revature/repository/UserRepository.java
@@ -13,4 +13,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
 
     boolean existsByUsername(String username);
+    
+    User getUserByUsername(String username);
 }
